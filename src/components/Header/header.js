c15b546142c45c1e19f18943111e0df27f160947
brunoeiji151 <brunoeiji151@gmail.com>
2024-03-04 16:43:05 -0300

    first commit, testando nav-bar

---
+++ src/components/Header/header.js
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import "../Header/header.css"
+
+function Header() {
+    const navRef = useRef();
+
+    const shadowNavBar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
+    return (
+        <header>
+            <h3>Logo</h3>
+            <nav ref={navRef}>
+                <a href="">Inicio</a>
+                <a href="">Experiência</a>
+                <a href="">Currículo</a>
+                <a href="">Sobre mim</a>
+                <button className='nav-btn nav-close-btn' onClick={shadowNavBar}>
+                    <FontAwesomeIcon icon={faTimes} />
+                </button>
+            </nav>
+            <button className='nav-btn' onClick={shadowNavBar}>
+                <FontAwesomeIcon icon={faBars} />
+            </button>
+        </header>
+    );
+}
+
+export default Header;
