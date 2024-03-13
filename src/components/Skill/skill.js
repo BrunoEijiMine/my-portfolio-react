e412b4f5b1681dbb6cb44a612d0484f78448c7a6
brunoeiji151 <brunoeiji151@gmail.com>
2024-03-13 18:05:04 -0300

    adicionando habilidades ao portfolio

--- src/components/Skill/skill.js
+++ src/components/Skill/skill.js
@@ -1,13 +1,41 @@
 import "../Skill/skill.css";
+import htmlIcon from "../../img/html.svg"
+import cssIcon from "../../img/css.svg"
+import jsIcon from "../../img/javascript.svg"
+import bootstrapIcon from "../../img/bootstrap.svg"
+import reactIcon from "../../img/react.svg"
+import psIcon from "../../img/photoshop.svg"
+import figmaIcon from "../../img/figma.svg"
 
 function Skill() {
+  
   return (
-    <section>
-        <h1 className="title-skill">HABILIDADES</h1>
-      <div className="card-skill">
-            <h3>teste</h3>
+    <div className="wrapper-s">
+      <h1 className="title-skill">HABILIDADES</h1>
+      <div className="container-skill">
+        <article className="skill">
+          <img src={htmlIcon} />
+        </article>
+        <article className="skill">
+          <img src={cssIcon} />
+        </article>
+        <article className="skill">
+          <img src={jsIcon} />
+        </article>
+        <article className="skill">
+          <img src={bootstrapIcon} />
+        </article>
+        <article className="skill">
+          <img src={reactIcon} />
+        </article>
+        <article className="skill">
+          <img src={psIcon} />
+        </article>
+        <article className="skill">
+          <img src={figmaIcon} />
+        </article>
       </div>
-    </section>
+    </div>
   );
 }
 
