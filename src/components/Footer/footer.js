94edd62a0a05be74f1903f8616fb3cce641e9a8d
brunoeiji151 <brunomine151@gmail.com>
2024-04-11 10:10:59 -0300

    update footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import Aos from "aos";
 import 'aos/dist/aos.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faGithub, faLinkedinIn, } from "@fortawesome/free-brands-svg-icons";
 import "../Footer/footer.css";
 
 function Footer() {
@@ -14,12 +16,33 @@ function Footer() {
             data-aos="zoom-in-up"
             data-aos-easing="linear">
             <div className="footer-title">
-                <p className="footer-back-to-home">TOPO</p>
-                <p className="footer-name">EIJI™</p>
+                <p className="footer-name">W3B D3S1GN</p>
             </div>
             <div className="container-footer">
-                <div>teste</div>
-                <div>teste</div>
+                <a href="https://www.instagram.com/brunoeiji1/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Instagram"
+                    data-aos="fade-up"
+                >
+                    <FontAwesomeIcon className="icon" icon={faInstagram} />
+                </a>
+                <a href="https://www.linkedin.com/in/bruno-eiji-1b47b1206/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="LinkedIn"
+                    data-aos="fade-up"
+                >
+                    <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
+                </a>
+                <a href="https://github.com/BrunoEijiMine"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Github"
+                    data-aos="fade-up"
+                >
+                    <FontAwesomeIcon className="icon" icon={faGithub} />
+                </a>
             </div>
         </div>
     );
