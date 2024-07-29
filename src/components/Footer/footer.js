8ec289a77fffb25caf37e80f0187d9ea257847ab
brunoeiji151 <brunomine151@gmail.com>
2024-07-29 16:43:16 -0300

    ajuste circle japan

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -13,17 +13,22 @@ function Footer() {
 
     return (
         <>
-            <div className="footer"
-                data-aos="zoom-in-up"
-                data-aos-easing="linear">
-                <div id="wrapper-circle"><span id="circle"></span></div>
+            <div className="footer" data-aos="fade">
+                <div id="wrapper-circle"
+                    data-aos="zoom-in-down"
+                    data-aos-easing="linear">
+                    <span id="circle"></span>
+                </div>
                 <div className="container-title">
-                    <p className="title-footer">CONTATO</p>
+                    <p className="title-footer"
+                        data-aos="zoom-in-up"
+                        data-aos-easing="linear">CONTATO</p>
                 </div>
             </div>
             <div className="container-footer">
                 <a href="https://www.instagram.com/brunoeiji1/"
                     target="_blank"
+                    data-aos="fade-up"
                     rel="noopener noreferrer"
                     title="Instagram"
                 >
@@ -31,6 +36,7 @@ function Footer() {
                 </a>
                 <a href="https://www.linkedin.com/in/bruno-eiji-1b47b1206/"
                     target="_blank"
+                    data-aos="fade-up"
                     rel="noopener noreferrer"
                     title="LinkedIn"
                 >
@@ -38,6 +44,7 @@ function Footer() {
                 </a>
                 <a href="https://github.com/BrunoEijiMine"
                     target="_blank"
+                    data-aos="fade-up"
                     rel="noopener noreferrer"
                     title="Github"
                 >
